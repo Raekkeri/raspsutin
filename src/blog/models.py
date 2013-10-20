80e7b1a2d0c90e510828f2da4c35e752cf6f5520
Teemu Husso <teemu.husso@gmail.com>
2013-10-20 10:25:46 +0300

    Update blog model, use django_extensions

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 from django.utils import timezone
+from django_extensions.db.fields import (
+        AutoSlugField,
+        CreationDateTimeField,
+        )
 
 
 class BlogEntry(models.Model):
     title = models.CharField(max_length=1024)
     text = models.TextField()
     publish_date = models.DateTimeField()
+    creation_date = CreationDateTimeField()
     is_public = models.BooleanField(default=False)
+    slug = AutoSlugField(populate_from='title')
 
     def save(self, *args, **kwargs):
         if not self.publish_date:
